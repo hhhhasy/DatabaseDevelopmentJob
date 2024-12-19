58973e6d9771491efddc96ffd92cf2cdb7e7c56a
Lorry <2830123710@qq.com>
2024-12-19 17:58:53 +0800

    增加ai经典算法介绍

---
+++ advanced/frontend/models/Aialgorithm.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace frontend\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "ai_algorithm".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string|null $description
+ * @property string|null $example_image
+ * @property string|null $animation
+ */
+class Aialgorithm extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'ai_algorithm';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['description'], 'string'],
+            [['name', 'example_image', 'animation'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'description' => 'Description',
+            'example_image' => 'Example Image',
+            'animation' => 'Animation',
+        ];
+    }
+}
